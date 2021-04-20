3479170559599750b728b7d7b5f7278aa237e95d
Alessandro <alessandro.lima@serpro.gov.br>
2021-04-20 10:36:34 -0300

    feat: Initial commit

---
+++ src/main/java/org/nitroxin/inboad/InboadApplication.java
@@ -0,0 +1,13 @@
+package org.nitroxin.inboad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nitroxin/inboad/InboadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nitroxin.inboad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
